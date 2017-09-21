c3b3d9ada54db008c296c25e8bf322d92bf8e8a6
William Calderipe <wcalder@thoughtworks.com>
2017-09-20 22:13:41 -0300

    Add PORT env var

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const app = require('app')
 
-app.listen(3000, () => {
-  console.log('server running at port 3000')
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`server running at port ${port}`)
 })
 
