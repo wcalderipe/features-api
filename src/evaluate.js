767d02c3765261e5a4d7ec9b7d7c3631d6bfedb5
William Calderipe <wcalder@thoughtworks.com>
2017-09-26 01:44:52 -0300

    Evaluate all feature parameters with AND operator

--- src/evaluate.js
+++ src/evaluate.js
@@ -1,19 +1,31 @@
-const {prop} = require('ramda')
+const {prop, propEq, chain, all, equals, pipe} = require('ramda')
 
 const evaluate = (context, features) => {
   let data = {}
   
   features.forEach((feature) => {
-    data[feature.name] = check(context, feature.parameters[0])
+   data[feature.name] = checkAll(context, feature.parameters) 
   })
-    
+
   return data
 }
 
-const check = (context, parameter) => {
-  const value = prop(parameter.name, context)
-  
-  return value === parameter.given
+const checkAll = (context, parameters) => {
+  const checkedParameters = chain(check(context))
+  const isAllSatisfied = all(equals(true))
+
+  return pipe(
+    checkedParameters,
+    isAllSatisfied
+  )(parameters)
+}
+
+const check = (context) => (parameter) => {
+  const {name, given} = parameter
+  const value = prop(name, context)
+  const isSatisfied = propEq(name, given)
+
+  return isSatisfied(context)
 }
 
 module.exports = {evaluate}
