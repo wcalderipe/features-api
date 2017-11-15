360bb98b575965c116859f4356ae1b08a1102a67
William Calderipe <wcalder@thoughtworks.com>
2017-11-15 17:31:39 -0300

    Add integration test to not found application

--- test/integration/togglesTest.js
+++ test/integration/togglesTest.js
@@ -1,5 +1,6 @@
-import {OK} from 'http-status'
+import {OK, NOT_FOUND} from 'http-status'
 import request from 'supertest'
+import {expect} from '../testSetup'
 import app from '../../src/app'
 
 describe('GET /toggles', () => {
@@ -15,6 +16,32 @@ describe('GET /toggles', () => {
       .get('/toggles')
       .query(queryParams)
       .expect(OK)
+  })
+
+  context('when application is not found', () => {
+    const queryParams = {
+      application: 'unkwonApp'
+    }
+
+    it('returns status 404', () => {
+      return request(app)
+        .get('/toggles')
+        .query(queryParams)
+        .expect(NOT_FOUND)
+    })
+
+    it('returns error code', () => {
+      const expectedBody = {
+        code: 'ERR_APPLICATION_NOT_FOUND'
+      }
+
+      return request(app)
+        .get('/toggles')
+        .query(queryParams)
+        .then((response) => {
+          expect(response.body).to.deep.equal(expectedBody)
+        })
+    })
   })
 })
 
