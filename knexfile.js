62ca7324057fdf6eff50c3dca6c9deee56ffaa31
William Calderipe <wcalder@thoughtworks.com>
2017-11-17 01:03:10 -0300

    Add knex with SQLite3 more first schema attempt

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './db/development.sqlite3'
+    },
+    migrations: {
+      directory: './db/migrations',
+      tableName: 'migrations'
+    },
+    seeds: {
+      directory: './db/seeds/test'
+    }
+  }
+}
