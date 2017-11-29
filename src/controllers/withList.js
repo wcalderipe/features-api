00ea2a12cb1cb77a818e399ac210d471de0df7a4
William Calderipe <wcalder@thoughtworks.com>
2017-11-29 01:29:30 -0300

    Create controller HOF withList

---
+++ src/controllers/withList.js
@@ -0,0 +1,14 @@
+import {mergeDeepRight} from 'ramda'
+
+const list = (repository) => async (req, res) => {
+  const entities = await repository.findAll()
+
+  return res.json(entities)
+}
+
+const withList = (repository) => (object) => mergeDeepRight(object, {
+  list: list.bind(null, repository)
+})
+
+export {withList}
+
---
+++ test/unit/controllers/withListTest.js
@@ -0,0 +1,25 @@
+import {pipe} from 'ramda'
+import td from 'testdouble'
+import {withList} from '../../../src/controllers/withList'
+
+describe('controller withList', () => {
+  const res = {
+    json: td.function()
+  }
+  const req = {}
+
+  it('calls res.json with applications list', async () => {
+    const fakeRepository = {
+      findAll: td.function()
+    }
+    const controller = pipe(
+      withList(fakeRepository)
+    )({})
+
+    td.when(fakeRepository.findAll()).thenResolve([])
+
+    await controller.list(fakeRepository)(req, res)
+
+    td.verify(res.json([]))
+  })
+})
