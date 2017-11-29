a104dd5c0ec1d16e9b04fa9153f70d01add48d1b
William Calderipe <wcalder@thoughtworks.com>
2017-11-29 00:22:45 -0300

    Add integration tests for withUpdate

---
+++ test/integration/repositories/composition/withUpdateTest.js
@@ -0,0 +1,37 @@
+import Knex from 'knex'
+import {pipe} from 'ramda'
+import {expect} from '../../../testSetup'
+import {withUpdate} from '../../../../src/repositories/composition'
+
+const knexfile = require('../../../../knexfile')
+const knex = Knex(knexfile.test)
+
+const TMP_TABLE_NAME = 'withUpdate_test_table'
+
+describe('withUpdate', () => {
+  before(async () => {
+    await knex.schema.createTable(TMP_TABLE_NAME, (table) => {
+      table.increments()
+      table.string('name')
+    })
+
+    await knex(TMP_TABLE_NAME).insert({id: 1, name: 'TmpEntity01'})
+  })
+
+  after(async () => {
+    await knex.schema.dropTable(TMP_TABLE_NAME)
+  })
+
+  it('updates an entity and returns the number of affect rows', async () => {
+    const repository = pipe(
+      withUpdate(knex, TMP_TABLE_NAME)
+    )({})
+
+    const affectedRows = await repository.update(1, {name: 'UpdatedName'})
+    const entity = await knex(TMP_TABLE_NAME).where({id: 1}).first()
+
+    expect(affectedRows).to.equal(1)
+    expect(entity.name).to.equal('UpdatedName')
+  })
+})
+
