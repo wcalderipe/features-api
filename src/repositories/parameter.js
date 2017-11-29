7171e49accf878ac03138843387365baee6fd3a0
William Calderipe <wcalder@thoughtworks.com>
2017-11-29 00:36:02 -0300

    Remove valuless tests of parameter repository

--- src/repositories/parameter.js
+++ src/repositories/parameter.js
@@ -1,9 +1,11 @@
+import {pipe} from 'ramda'
+import {withFindAll} from './composition'
+
 const TABLE_NAME = 'parameters'
 
-const parameterRepository = (knex) => ({
-  findAll: findAll.bind(null, knex)
-})
-
-const findAll = (knex) => knex(TABLE_NAME)
+const parameterRepository = (knex) => pipe(
+  withFindAll(knex, TABLE_NAME)
+)({})
 
 export {parameterRepository}
+
